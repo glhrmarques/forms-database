f57359f9865adac9218f2306306851896e715107
glhrmarques <gmarques.pd@gmail.com>
2026-01-29 20:07:53 -0300

    get and post

--- db.js
+++ db.js
@@ -1,27 +1,45 @@
+import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv'
 import express from 'express';
-import dotenv from 'dotenv';
-import postgres from 'postgres'
 
 dotenv.config()
 
-const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString)
+const app = express()
+const PORT = 8080
 
-export default sql
+const supabaseUrl = 'https://nxuehljrynbmwceqklxa.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
 
-const app = express();
-const PORT = 8080;
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+app.post('/senddata', async (req,res) => {
+
+    const today = new Date();
+
+    const { data, error} = await supabase
+    .from('people')
+    .insert([
+        {id: 1234, name: 'Camila', current_date: today},
+        {id: 6789, name: 'Mario', current_date: today},
+    ]);
+
+    if (error) {
+        res.status(500).json({error: error.message});
+    } else {
+        res.json(data)
+    }
+});
 
 app.get('/', async (req,res) => {
-    try {
-        const response = await sql`SELECT "prospect-name", "propspect-phone-number" FROM propects`;        
-        res.json(response);
+    const { data, error } = await supabase.from('people').select('*')
 
-    } catch (error) {
-        console.error(error.message)
+    if (error) {
+        res.status(500).json({error: error.message})
+    } else {
+        res.json(data)
     }
-
 });
 
 
-app.listen(PORT, () => console.log(`Server running, http://localhost:${PORT}`));
+app.listen(PORT, console.log(`Server is running in http://localhost:${PORT}`));
